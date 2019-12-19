ff708245e6e5a053c8f5a6627fb9105158559f62
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2019-12-19 18:47:21 +0900

    User-Views

--- app/controllers/threads_controller.rb
+++ app/controllers/threads_controller.rb
@@ -1,4 +1,8 @@
 class ThreadsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'thread#index'
+  root 'threads#index'
   resources :users
   resources :threads
 end
